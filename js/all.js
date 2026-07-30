4b898f5feaa7dd3f53211fac5e31440241dc3e61
Chiuan.Z <jjojo168tw@gmail.com>
2026-07-30 16:36:38 +0800

    0730update

--- js/all.js
+++ js/all.js
@@ -2,14 +2,13 @@
 
 $(document).ready(function() {
     
-    // fontSize
+    // [響應式字體大小設定]依視窗寬度動態設定html根字體大小 => 1920px設計稿時，1rem=100px，最小以 1000px 計算），讓 rem 版面隨螢幕等比例縮放
     function fontSize() {
       var a = document.documentElement.clientWidth < 1000 ? 1000 : document.documentElement.clientWidth;
       document.documentElement.style.fontSize = (a / 19.2) + "px"
     }
     fontSize();
-    window.onresize = fontSize;
-
+    window.addEventListener('resize', fontSize);
 
     // sideBar
     $('.switch').click(function(e) {
@@ -81,34 +80,29 @@ $(document).ready(function() {
 
 
     // scroll fadeIn
-    $(window).scroll(function(){
-        var scrollPos = $(window).scrollTop();
-        var windowHeight = $(window).height(); 
+    // $(window).scroll(function(){
+    //     var scrollPos = $(window).scrollTop();
+    //     var windowHeight = $(window).height(); 
         
-        // sideBar
-        if ($(window).scrollTop() > 400) {
-            $('aside').fadeIn(500);
-          } else {
-            $('aside').fadeOut(500);
-        };
+    //     // sideBar
+    //     if ($(window).scrollTop() > 1200) {
+    //         $('aside').fadeIn(500);
+    //       } else {
+    //         $('aside').fadeOut(500);
+    //     };
 
-        // section - title & text
-        $('.hide').each(function(){
-        var thisPos = $(this).offset().top;
-        if((windowHeight + scrollPos) >= thisPos + 300) {
-        $(this).addClass('fadeIn');
-        }
-        }); 
+    //     // section - title & text
+    //     $('.hide').each(function(){
+    //     var thisPos = $(this).offset().top;
+    //     if((windowHeight + scrollPos) >= thisPos + 300) {
+    //     $(this).addClass('fadeIn');
+    //     }
+    //     }); 
    
     // Create a MediaQueryList object
     var x = window.matchMedia('(max-width: 999px)')
         
     });  
-
-  
-
-}); 
-
 
 
 
